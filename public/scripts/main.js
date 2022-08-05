0676cb9c1a1b22f03d1e934baa898c4bad4b5490
MaweniLive <saliwalive@gmailcom>
2022-08-05 10:12:11 +0200

    woork

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -16,23 +16,3 @@ fetch("http://localhost:6969/users")
     });
   });
 
-      //fetch products data and display
-            fetch("http://localhost:6969/products")
-        .then((res) => res.json())
-        .then((data) => {
-          data.forEach((i) => {
-            document.querySelector(".body").innerHTML += `<tr>
-                 <td>${i.product_id}</td>
-                 <td>${i.sku}</td>
-                 <td>${i.name}</td>
-                 <td>${i.price}</td>
-                 <td>${i.weight}</td>
-                 <td>${i.descriptions}</td>
-                 <td>${i.thumbnail}</td>
-                 <td>${i.image}</td>
-                 <td>${i.category}</td>
-                 <td>${i.create_date}</td>
-                 <td>${i.stock}</td>
-                 </tr>`;
-          });
-        });
